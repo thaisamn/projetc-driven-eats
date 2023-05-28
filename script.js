e9f9087c5ed8ec4fa0fab4b41b3d323e147307da
thaisamn <thaisa.umes@gmail.com>
2023-05-27 21:44:55 -0300

    corrigindo o que foi indicado

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ const produtos_selecionados = {};
 
 function selecionarProduto(categoria, posicao, produto, preco) {
    const items = document.querySelectorAll('div#' + categoria + ' > div.item');
+   const checks = document.querySelectorAll('div#' + categoria + ' > div.item * img.check');
    for (let count = 0; count < items.length; count++) {
       const element = items[count];
+      const check = checks[count]
       if (count == posicao) {
          element.classList.add('borda');
+         check.classList.remove('escondido')
       } else {
          element.classList.remove('borda');
+         check.classList.add('escondido')
       }
    };
 
    produtos_selecionados[categoria] = {
       produto,
-      preco
+      preco,
+      categoria
    };
 
    if (Object.keys(produtos_selecionados).length === 3) {
@@ -56,16 +61,20 @@ function enviarPedido(){
    const produtos = Object.keys(produtos_selecionados).map(key => {
       return produtos_selecionados[key];
    });
+   const nome = prompt('digite seu nome');
+   const endereco = prompt('digite seu endereço');
+
    let total = 0;
 
    let resumo = 'Olá, gostaria de fazer o pedido:\n'
    produtos.forEach(p => {
       total += p.preco;
-      resumo += `- ${p.produto}\n`
+      resumo += `- ${p.categoria}: ${p.produto}\n`
    })
 
-   resumo += `Total: R$${total.toFixed(2)}`
+   resumo += `Total: R$ ${total.toFixed(2).replace('.', ',')}\n\n`
 
+   resumo += `Nome: ${nome}\nEndereço: ${endereco}`
 
    window.open(`https://wa.me/5511932291668?text=${encodeURIComponent(resumo)}`)
    
